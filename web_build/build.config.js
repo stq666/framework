5fc2014975b611a8fdc168e40ffdd9f4adaee387
ubuntu <stqsht@163.com>
2016-10-18 11:55:13 +0800

    framework

---
+++ web_build/build.config.js
@@ -0,0 +1,18 @@
+'use strict';
+module.exports = {
+    /**
+     * The `build_dir` folder is where our projects are compiled during
+     * development and the `compile_dir` folder is where our app resides once it's
+     * completely built.
+     */
+    //  build_dir: '../CloudAppMobile/www',
+    orig_dir: 'bower_components',
+
+    css_dir:'../web/src/main/webapp/css',
+    js_dir:'../web/src/main/webapp/js',
+    img_dir:'../web/src/main/webapp/images',
+    font_dir:'../web/src/main/webapp/fonts',
+    module_js:'../web/src/main/webapp/module/',
+    index:'../web/src/main/webapp/',
+
+}
